a0a5991ab6864209880af9933ffa5ba597f3de8e
Arnol Fokam <arnolfokam23@gmail.com>
2022-03-12 20:19:44 +0100

    fist commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()        
+        # N, 1, 28, 28
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=2, padding=1), # -> N, 16, 14, 14
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1), # -> N, 32, 7, 7
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 7) # -> N, 64, 1, 1
+        )
+        
+        # N , 64, 1, 1
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, 7), # -> N, 32, 7, 7
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), # N, 16, 14, 14 (N,16,13,13 without output_padding)
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1), # N, 1, 28, 28  (N,1,27,27)
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        encoded = self.encode(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    def encode(self, x):
+        encoded = self.encoder(x)
+        return encoded
+
+def train():
+    transform = transforms.ToTensor()
+    mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    data_loader = torch.utils.data.DataLoader(dataset=mnist_data,
+                                            batch_size=64,
+                                            shuffle=True)
+
+    model = Autoencoder()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(),
+                                lr=1e-3, 
+                                weight_decay=1e-5)
+
+    # Point to training loop video
+    num_epochs = 10
+    outputs = []
+    for epoch in range(num_epochs):
+        for (img, _) in data_loader:
+            # img = img.reshape(-1, 28*28) # -> use for Autoencoder_Linear
+            recon = model(img)
+            loss = criterion(recon, img)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
+        outputs.append((epoch, img, recon))
