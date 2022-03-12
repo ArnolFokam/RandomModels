278f664a0b6fc1613d68d8ca61689ee23c2a50a6
Arnol Fokam <arnolfokam23@gmail.com>
2022-03-12 20:21:06 +0100

    add embedding plot

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
+import numpy as np
 import matplotlib.pyplot as plt
+import umap
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 class Autoencoder(nn.Module):
     def __init__(self):
@@ -63,3 +67,51 @@ def train():
 
         print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
         outputs.append((epoch, img, recon))
+
+    for k in range(0, num_epochs, 4):
+        plt.figure(figsize=(9, 2))
+        plt.gray()
+        imgs = outputs[k][1].detach().numpy()
+        recon = outputs[k][2].detach().numpy()
+        for i, item in enumerate(imgs):
+            if i >= 9: break
+            plt.subplot(2, 9, i+1)
+            # item = item.reshape(-1, 28,28) # -> use for Autoencoder_Linear
+            # item: 1, 28, 28
+            plt.imshow(item[0])
+                
+        for i, item in enumerate(recon):
+            if i >= 9: break
+            plt.subplot(2, 9, 9+i+1) # row_length + i + 1
+            # item = item.reshape(-1, 28,28) # -> use for Autoencoder_Linear
+            # item: 1, 28, 28
+            plt.imshow(item[0])
+
+    plt.savefig("fig.png")
+
+    print("plotting embeddings")
+    outputs_emb = []
+    outputs_label = []
+
+    for (img, _) in data_loader:
+        # img = img.reshape(-1, 28*28) # -> use for Autoencoder_Linear
+        outputs_emb.append(model.encode(img).detach().numpy())
+        outputs_label.append(_.detach().numpy())
+
+    outputs_emb_ex = np.squeeze(outputs_emb[0])
+    outputs_label_ex = outputs_label[0]
+
+    scaled_emb = StandardScaler().fit_transform(outputs_emb_ex)
+    reducer = umap.UMAP()
+    embedding = reducer.fit_transform(scaled_emb)
+
+    for g in np.unique(outputs_label_ex):
+        ix = np.where(outputs_label_ex == g)
+        plt.scatter(
+            embedding[ix, 0],
+            embedding[ix, 1],
+            c=[sns.color_palette()[g]],
+            label=g)
+    plt.gca().set_aspect('equal', 'datalim')
+    plt.legend()
+    plt.title('UMAP projection of the mnist dataset', fontsize=24)
